a92950ac322c1583090d20076c3c502c4b44efe8
Timur <khalilov.timur97@mail.ru>
2021-04-17 13:42:39 +0600

    p1

--- products/views.py
+++ products/views.py
@@ -1,19 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
+from django.views.generic import ListView, DetailView, CreateView
 
 
 def index(request):
     return render(request, 'base.html')
 
-# def post_list(request):
-#     return render(request, 'base.html', {})
-
-# class ProdHome(ListView):
+#
+# class ProducerHome(ListView):
 #     model = Producer
 #     template_name = 'base.html'
 #     context_object_name = 'producer'
 #
 #     def get_context_data(self, *, object_list=None, **kwargs):
 #         context = super().get_context_data(**kwargs)
-#         context['title'] = self.get_upper('Главная страница')
+#         context['name'] = self.get_upper('Главная страница')
 #         return context
+#
+#
+# class Category(ListView):
+#     model = ProductType
+#     template_name = 'base.html'
+#     context_object_name = 'category'
+#     allow_empty = False
+#
+#     def get_context_data(self, *, object_list=None, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['title'] = self.get_upper(ProductType.objects.get(pk=self.kwargs[
+#              'category_id']))
+#         return context
+#
+#
+# class ProdHome(ListView):
+#     model = Product
+#     template_name = 'base.html'
+#     context_object_name = 'product'
